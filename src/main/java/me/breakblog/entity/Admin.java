1dca5e38c69c458a58d8aed5f91f14ed049d64da
tw-huang <tw.huang@foxmail.com>
2020-07-19 01:40:28 +0800

    #添加lombok#优化代码#

--- src/main/java/me/breakblog/entity/Admin.java
+++ src/main/java/me/breakblog/entity/Admin.java
@@ -1,92 +1,25 @@
 package me.breakblog.entity;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class Admin implements Serializable {
+
     private Integer id;
+
     private String username;
+
     private String password;
+
     private String blogTitle;
+
     private String blogSubtitle;
+
     private String name;
+
     private String about;
+
     private String email;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getBlogTitle() {
-        return blogTitle;
-    }
-
-    public void setBlogTitle(String blogTitle) {
-        this.blogTitle = blogTitle;
-    }
-
-    public String getBlogSubtitle() {
-        return blogSubtitle;
-    }
-
-    public void setBlogSubtitle(String blogSubtitle) {
-        this.blogSubtitle = blogSubtitle;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAbout() {
-        return about;
-    }
-
-    public void setAbout(String about) {
-        this.about = about;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "Admin{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", blogTitle='" + blogTitle + '\'' +
-                ", blogSubtitle='" + blogSubtitle + '\'' +
-                ", name='" + name + '\'' +
-                ", about='" + about + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
--- src/main/java/me/breakblog/entity/Link.java
+++ src/main/java/me/breakblog/entity/Link.java
@@ -1,42 +1,16 @@
 package me.breakblog.entity;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class Link implements Serializable {
+
     private Integer id;
+
     private String name;
+
     private String url;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    @Override
-    public String toString() {
-        return "Link{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", url='" + url + '\'' +
-                '}';
-    }
 }
