62f50d3101de9123158aacb5fc2ad8994a7720cd
tw.huang <tw.huang@foxmail.com>
2021-05-12 23:13:20 +0800

    #异常处理#

--- src/main/java/me/breakblog/config/exception/ResultAdviceExceptionHandler.java
+++ src/main/java/me/breakblog/config/exception/ResultAdviceExceptionHandler.java
@@ -3,6 +3,7 @@ package me.breakblog.config.exception;
 import lombok.extern.slf4j.Slf4j;
 import me.breakblog.util.Result;
 import me.breakblog.util.ResultEnum;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,6 +21,17 @@ public class ResultAdviceExceptionHandler {
     public Result handleTokenException(TokenException e) {
         log.error(e.getMessage(), e);
         return Result.failure(ResultEnum.UNAUTHORIZED.getCode(), ResultEnum.UNAUTHORIZED.getMsg());
+    }
+
+    @ExceptionHandler(BindException.class)
+    public Result handleBindException(BindException e){
+        log.error(e.getMessage(), e);
+        StringBuffer message = new StringBuffer();
+        e.getAllErrors().forEach(error -> {
+            message.append(error.getDefaultMessage()).append(",");
+        });
+        String result = message.toString();
+        return Result.failure(result.substring(0, result.length() - 1));
     }
 
 }
