5a4f82d9b89c66f2d706c9cc7f5ed51177ab632b
tw.huang <tw.huang@foxmail.com>
2021-08-27 05:18:16 +0800

    #ui#接入api#

--- breakblog-ui/src/pages/Home/index.tsx
+++ breakblog-ui/src/pages/Home/index.tsx
@@ -1,11 +1,99 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import dayjs from 'dayjs'
 import './index.css'
 import Header from '../../compents/Header'
 import Footer from '../../compents/Footer'
 import PostImg from '../../assets/post.jpg'
 import Banner from '../../compents/Banner'
+import {
+	fetchLinks,
+	fetchCategories,
+	fetchPostsHot,
+	fetchPosts,
+} from '../../services'
+
+interface Link {
+	id: number
+	createTime: string
+	updateTime: string
+	name: string
+	url: string
+}
+
+interface Category {
+	id: number
+	createTime: string
+	updateTime: string
+	name: string
+	postCount: number
+}
+
+interface Post {
+	id: number
+	createTime: string
+	updateTime: string
+	title: string
+	subtitle: string
+	body: string
+	timestamp: string
+	canComment: boolean
+	pageView: number
+	category: Category
+}
 
 const Home: React.FC = () => {
+	// 搜索文本
+	const [searchText, setSearchText] = useState<string>('')
+	// 搜索文本 ref
+	const searchEl = useRef(null)
+	// 友链
+	const [links, setLinks] = useState<Array<Link>>([])
+	// 分类
+	const [categories, setCategories] = useState<Array<Category>>([])
+	// 热门文章
+	const [postsHot, setPostsHot] = useState<Array<Post>>([])
+	// 文章
+	const [posts, setPosts] = useState<Array<Post>>([])
+
+	// 监听搜索回车按键事件
+	useEffect(() => {
+		const handleSearchEvent = (event: { keyCode: number }) => {
+			const { keyCode } = event
+			if (keyCode === 13) {
+				// 键盘回车键
+				console.log(searchText)
+			}
+		}
+		document.addEventListener('keyup', handleSearchEvent)
+		return () => {
+			document.removeEventListener('keyup', handleSearchEvent)
+		}
+	})
+
+	const fetchData = async () => {
+		const links = await fetchLinks()
+		if (links?.success && links.code === 1) {
+			setLinks(links.data)
+		}
+		const categories = await fetchCategories()
+		if (categories?.success && categories.code === 1) {
+			setCategories(categories.data)
+		}
+		const postsHot = await fetchPostsHot()
+		if (postsHot?.success && postsHot.code === 1) {
+			setPostsHot(postsHot.data)
+		}
+		const posts = await fetchPosts('', null, 1, 10)
+		if (posts?.success && posts.code === 1) {
+			setPosts(posts.data.records)
+		}
+	}
+
+	useEffect(() => {
+		//获取初始数据
+		fetchData()
+	}, [])
+
 	return (
 		<div className='md:max-w-screen-lg w-full md:my-8 md:mx-auto bg-white '>
 			{/* 顶部 */}
@@ -13,96 +101,43 @@ const Home: React.FC = () => {
 			<div className='flex flex-col md:flex-row'>
 				<div className='md:px-8 p-2 md:w-3/4 md:py-6'>
 					{/* 轮播图 */}
-					<Banner/>
+					<Banner />
 					{/* 文章列表 */}
 					<div>
-						<div className='flex md:pt-4 pt-2'>
-							<div
-								className='pr-2 hidden md:block '
-								style={{ minWidth: '200px' }}
-							>
-								<img src={PostImg} alt='jpg' />
-							</div>
-							<div className='flex flex-col'>
-								<a
-									href='http://www.baidu.com'
-									className='text-xl md:text-2xl pb-2'
-								>
-									Java性能测试世界
-								</a>
-								<span className='text-xs pb-2 text-gray-500'>
-									分类：Java 日期：2015-08-12 点击数：1120
-								</span>
-								<span className='text-sm pb-2'>
-									风一程、雨一程，为那舌尖美食；却意外收尽旖旎、迷人；车几刻、行几刻；埔港深巷如烟迷蒙；是戴望舒的“雨巷”？“撑着油纸伞，独自彷徨在悠长悠长，又寂寥的雨巷，我希望逢着一个”……车如流水，人如织；喧哗让我莫遐思……
-								</span>
-								<a
-									href='http://www.baidu.com'
-									className='text-sm text-gray-500'
-									style={{ alignSelf: 'flex-end' }}
-								>
-									阅读正文-&gt;
-								</a>
-							</div>
-						</div>
-						<div className='flex md:pt-4 pt-2'>
-							<div
-								className='pr-2 hidden md:block '
-								style={{ minWidth: '200px' }}
-							>
-								<img src={PostImg} alt='jpg' />
-							</div>
-							<div className='flex flex-col'>
-								<a
-									href='http://www.baidu.com'
-									className='text-xl md:text-2xl pb-2'
-								>
-									Java性能测试世界
-								</a>
-								<span className='text-xs pb-2 text-gray-500'>
-									分类：Java 日期：2015-08-12 点击数：1120
-								</span>
-								<span className='text-sm pb-2'>
-									风一程、雨一程，为那舌尖美食；却意外收尽旖旎、迷人；车几刻、行几刻；埔港深巷如烟迷蒙；是戴望舒的“雨巷”？“撑着油纸伞，独自彷徨在悠长悠长，又寂寥的雨巷，我希望逢着一个”……车如流水，人如织；喧哗让我莫遐思……
-								</span>
-								<a
-									href='http://www.baidu.com'
-									className='text-sm text-gray-500'
-									style={{ alignSelf: 'flex-end' }}
-								>
-									阅读正文-&gt;
-								</a>
-							</div>
-						</div>
-						<div className='flex md:pt-4 pt-2'>
-							<div
-								className='pr-2 hidden md:block '
-								style={{ minWidth: '200px' }}
-							>
-								<img src={PostImg} alt='jpg' />
-							</div>
-							<div className='flex flex-col'>
-								<a
-									href='http://www.baidu.com'
-									className='text-xl md:text-2xl pb-2'
-								>
-									Java性能测试世界
-								</a>
-								<span className='text-xs pb-2 text-gray-500'>
-									分类：Java 日期：2015-08-12 点击数：1120
-								</span>
-								<span className='text-sm pb-2'>
-									风一程、雨一程，为那舌尖美食；却意外收尽旖旎、迷人；车几刻、行几刻；埔港深巷如烟迷蒙；是戴望舒的“雨巷”？“撑着油纸伞，独自彷徨在悠长悠长，又寂寥的雨巷，我希望逢着一个”……车如流水，人如织；喧哗让我莫遐思……
-								</span>
-								<a
-									href='http://www.baidu.com'
-									className='text-sm text-gray-500'
-									style={{ alignSelf: 'flex-end' }}
-								>
-									阅读正文-&gt;
-								</a>
-							</div>
-						</div>
+						{posts.map((post: Post) => {
+							return (
+								<div className='flex md:pt-4 pt-2' key={post.id}>
+									<div
+										className='pr-2 hidden md:block md:w-1/3'
+									>
+										<img src={PostImg} alt='jpg'/>
+									</div>
+									<div className='flex flex-col md:w-2/3 w-full'>
+										<a
+											href='http://www.baidu.com'
+											className='text-xl md:text-2xl pb-2'
+										>
+											{post.title}
+										</a>
+										<span className='text-xs pb-2 text-gray-500'>
+											分类：{post.category.name} 日期：
+											{dayjs(post.timestamp).format('YYYY-MM-DD')} 点击数：
+											{post.pageView}
+										</span>
+										<span className='text-sm pb-2'>
+											{post.subtitle}
+										</span>
+										<a
+											href='http://www.baidu.com'
+											className='text-sm text-gray-500'
+											style={{ alignSelf: 'flex-end' }}
+										>
+											阅读正文-&gt;
+										</a>
+									</div>
+								</div>
+							)
+						})}
 					</div>
 					{/* 分页 */}
 					<div className='flex justify-between md:pt-8 pt-4 underline'>
@@ -118,6 +153,9 @@ const Home: React.FC = () => {
 								type='text'
 								className='w-full border-2 border-back'
 								placeholder='请输入搜索的关键词'
+								value={searchText}
+								onChange={(event) => setSearchText(event.target.value)}
+								ref={searchEl}
 							/>
 						</label>
 					</div>
@@ -125,63 +163,47 @@ const Home: React.FC = () => {
 					<div className='md:mt-4 mt-2'>
 						<span className='text-lg'>热门文章:</span>
 						<div className='flex flex-col text-sm mt-1'>
-							<div className='flex flex-col pb-1'>
-								<a href='http://www.baidu.com' className='pb-1'>Java性能测试世界</a>
-								<span className='text-xs text-gray-500'>
-									日期：2015-08-12 点击数：1120
-								</span>
-							</div>
-							<div className='flex flex-col pb-1'>
-								<a href='http://www.baidu.com' className='pb-1'>Java性能测试世界</a>
-								<span className='text-xs text-gray-500'>
-									日期：2015-08-12 点击数：1120
-								</span>
-							</div>
-							<div className='flex flex-col pb-1'>
-								<a href='http://www.baidu.com' className='pb-1'>Java性能测试世界</a>
-								<span className='text-xs text-gray-500'>
-									日期：2015-08-12 点击数：1120
-								</span>
-							</div>
-							<div className='flex flex-col pb-1'>
-								<a href='http://www.baidu.com' className='pb-1'>Java性能测试世界</a>
-								<span className='text-xs text-gray-500'>
-									日期：2015-08-12 点击数：1120
-								</span>
-							</div>
+							{postsHot.map((post: Post) => {
+								return (
+									<div className='flex flex-col pb-1' key={post.id}>
+										<a href='http://www.baidu.com' className='pb-1'>
+											{post.title}
+										</a>
+										<span className='text-xs text-gray-500'>
+											日期：{dayjs(post.timestamp).format('YYYY-MM-DD')}{' '}
+											点击数：{post.pageView}
+										</span>
+									</div>
+								)
+							})}
 						</div>
 					</div>
 					{/* 文章分类 */}
 					<div className='md:mt-4 mt-2'>
 						<span className='text-lg'>文章分类:</span>
 						<div className='flex flex-col text-sm mt-1'>
-							<span className='pb-1'>
-								<a href='http://www.baidu.com'>Java(6)</a>
-							</span>
-							<span className='pb-1'>
-								<a href='http://www.baidu.com'>生活(7)</a>
-							</span>
-							<span className='pb-1'>
-								<a href='http://www.baidu.com'>Linux(7)</a>
-							</span>		<span className='pb-1'>
-								<a href='http://www.baidu.com'>分享(7)</a>
-							</span>
-
-					</div>
+							{categories.map((category: Category) => {
+								return (
+									<span className='pb-1' key={category.id}>
+										<a href='http://www.baidu.com'>
+											{category.name}({category.postCount})
+										</a>
+									</span>
+								)
+							})}
+						</div>
 					</div>
 					{/* 友情链接 */}
 					<div className='md:mt-4 mt-2'>
 						<span className='text-lg'>友情链接:</span>
 						<div className='flex flex-col text-sm mt-1'>
-							<span className='pb-1'>
-								<a href='http://www.baidu.com'>Github</a>
-							</span>
-							<span className='pb-1'>
-								<a href='http://www.baidu.com'>Gitee</a>
-							</span>
-							<span className='pb-1'>
-								<a href='http://www.baidu.com'>导航狗</a>
-							</span>
+							{links.map((link: Link) => {
+								return (
+									<span className='pb-1' key={link.id}>
+										<a href={link.url}>{link.name}</a>
+									</span>
+								)
+							})}
 						</div>
 					</div>
 				</div>
